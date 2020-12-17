ee6605edda0e0967f058fc732ecf60918fb2553b
cleds <42468523+cleds@users.noreply.github.com>
2020-12-17 20:27:45 +0300

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL #initializing inverse as NULL
 
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){  # Checking whether inverse is NULL
+    message("getting cached data")
+    return(inv) # return inverse value
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...) #calculate inverse value
+  x$setInverse(inv)
+  inv
 }
 
 
